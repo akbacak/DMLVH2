a944fa398983dfdc5d46870c3e77c66598c36336
akbacak <33653031+akbacak@users.noreply.github.com>
2019-11-13 16:21:33 +0300

    Add files via upload

---
+++ plot_PP.m
@@ -0,0 +1,16 @@
+clear all;
+close all;
+clc;
+
+ X = rand(200,2);
+ 
+   maxFront = 3;
+        
+       [pf_idx] = pareto_fronts(X, maxFront);
+        
+        
+ plot(X(:,1),X(:,2), 'k*'); hold on
+       
+ legend({'First Pareto Points'},'Location','southwest', 'FontSize', 28)      
+       
+      
